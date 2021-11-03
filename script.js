1958f78f8cc842a2f6f019edfca85ff45a5e3568
CyberOmerkhan <amazingOmerkhan@gmail.com>
2021-11-03 12:22:44 +0600

    v1.1

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 const input = document.getElementById('input');
-const btn = document.getElementById('btn');
-const res = document.querySelector('.res');
-let ToDoList = [];
-btn.addEventListener('click', addPoint);
-function addPoint(){
-    let value = input.value;
-    ToDoList.push(value);
-    document.getElementById('input').value = null;
-    show();
-}
-function show(){
-    let value = '';
-    for(const elements of ToDoList){
-        value = elements;
+const btn = document.querySelector('button');
+const result = document.getElementsByClassName('result')[0];
+const total = document.getElementById('total');
+let i = 0;
+btn.addEventListener('click', (e) => {
+    if(input.value.trim()){
+        i++;
+        console.log(input.value);
+        createElement(input.value);
+        input.value = '';
     }
-    res.innerHTML += `
-        <li>${value}</li>
-    `;
-    console.log(1);
+});
+function createElement(value){
+    total.textContent = `Total: ${i}`;
+    let li = document.createElement('li');
+    let btn = document.createElement('button');
+    li.textContent = value;
+    btn.textContent = 'delete';
+    li.className = 'li';
+    btn.className = 'bttn';
+    li.appendChild(btn);
+    result.appendChild(li);
+    btn.addEventListener('click', (e) => {
+        btn.parentNode.parentNode.removeChild(btn.parentNode);
+        i--;
+        total.textContent = `Total: ${i}`;
+    });
+    total.textContent = `Total: ${i}`;
 }
