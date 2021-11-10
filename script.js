232e4e26b462a5fec29360b23c081bcd49acc9a0
CyberOmerkhan <amazingOmerkhan@gmail.com>
2021-11-10 17:29:16 +0600

    Edited

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(() => {
     $('#btn').bind('click', () => {
         let value = $('#input').val();
-        $('ol.result').append(`<li>${value}</li>);
+        $('ol.result').append(`<li>${value}</li>`);
+        value = null;
     })
 })
